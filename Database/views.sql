4b21c1c97af3e11b92cd0742c1a8920836c8a2da
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-05-23 13:17:34 -0600

    Update views to limit leaderboards to first 10 registers

--- Database/views.sql
+++ Database/views.sql
@@ -2,29 +2,33 @@
 CREATE VIEW top_kills AS
 SELECT users.user_id, users.username, metrics.kills
 FROM users
-INNER JOIN metrics ON users.user_id = metrics.user_id
-ORDER BY metrics.kills DESC;
+INNER JOIN metrics USING (user_id)
+ORDER BY metrics.kills DESC
+LIMIT 10;
 
 -- K/D View
 CREATE VIEW top_kd AS
 SELECT users.user_id, users.username, metrics.kills, metrics.deaths
 FROM users
-INNER JOIN metrics ON users.user_id = metrics.user_id
-ORDER BY metrics.kills/metrics.deaths DESC;
+INNER JOIN metrics USING (user_id)
+ORDER BY metrics.kills/metrics.deaths DESC
+LIMIT 10;
+
 
 -- Win Rate View
 CREATE VIEW top_winrate AS
 SELECT users.user_id, users.username, metrics.wins, metrics.deaths
 FROM users
-INNER JOIN metrics ON users.user_id = metrics.user_id
-ORDER BY metrics.wins/metrics.deaths DESC;
+INNER JOIN metrics USING (user_id)
+ORDER BY metrics.wins/metrics.deaths DESC
+LIMIT 10;
 
 -- Weekly Eliminations Leaderboard 
 -- (You only need to play that week to appear on the leaderboard)
 CREATE VIEW top_weekly_elims AS
 SELECT users.user_id, users.username, metrics.kills
 FROM users
-INNER JOIN metrics ON users.user_id = metrics.user_id
+INNER JOIN metrics USING (user_id)
 WHERE metrics.last_update >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY)
 ORDER BY metrics.kills DESC;
 
@@ -32,12 +36,12 @@ ORDER BY metrics.kills DESC;
 CREATE VIEW death_place AS
 SELECT users.user_id, users.username, deaths.room
 FROM users
-INNER JOIN deaths ON deaths.user_id = user.user_id
+INNER JOIN deaths USING (user_id)
 ORDER BY deaths.room;
 
 -- Cause of Death View (Which type of character killed the player)
 CREATE VIEW death_cause AS
-SELECT users.user_id, users, users.username, deaths.killer
+SELECT users.user_id, users.username, deaths.killer
 FROM users
-INNER JOIN deaths on deaths.user_id = user.user_id
+INNER JOIN deaths USING (user_id)
 ORDER BY deaths.killer;
