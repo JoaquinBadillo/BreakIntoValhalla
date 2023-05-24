8de94ad1ff231d9078e8a7c27c74fdcdf81f0fed
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-05-23 19:40:35 -0600

    Commit manually on the data sql script

--- Database/valhalla-data.sql
+++ Database/valhalla-data.sql
@@ -18,8 +18,9 @@ INSERT INTO users (`username`, `email`, `password`) VALUES
 ('user5', 'user5@test.com', 'password'),
 ('user6', 'user6@test.com', '123456'),
 ('thor', 'odins@favourite.com', 'jotunHater1');
+COMMIT;
 
-
+SET AUTOCOMMIT=0;
 INSERT INTO metrics (`user_id`, `kills`, `deaths`, `wins`) VALUES
 (1, 100, 5, 2),
 (2, 50, 10, 0),
@@ -28,7 +29,9 @@ INSERT INTO metrics (`user_id`, `kills`, `deaths`, `wins`) VALUES
 (5, 150, 5, 3),
 (6, 50, 15, 0),
 (7, 1000, 0, 100);
+COMMIT;
 
+SET AUTOCOMMIT=0;
 INSERT INTO levels (`seed`) VALUES
 (123456),
 (654321),
@@ -37,12 +40,16 @@ INSERT INTO levels (`seed`) VALUES
 (456123),
 (789456),
 (159753);
+COMMIT;
 
+SET AUTOCOMMIT=0;
 INSERT INTO classes (`name`) VALUES
 ('Archer'),
 ('Berserker'),
 ('Spellcaster');
+COMMIT;
 
+SET AUTOCOMMIT=0;
 INSERT INTO games (`user_id`, `level_id`, `class_id`, `enemy_stats_multiplier`) VALUES
 (1, 1, 1, 1),
 (2, 2, 2, 1),
@@ -51,12 +58,16 @@ INSERT INTO games (`user_id`, `level_id`, `class_id`, `enemy_stats_multiplier`) 
 (5, 5, 2, 1),
 (6, 6, 3, 1),
 (7, 1, 1, 1);
+COMMIT;
  
+SET AUTOCOMMIT=0; 
 INSERT INTO stats (`class_id`, `hp`, `attack`, `attack_speed`, `defense`, `speed`) VALUES
 (1, 100, 10, 1.0, 5, 15.0),
 (2, 200, 25, 1.5, 1, 20.0),
 (3, 100, 15, 1.0, 2, 10.0);
+COMMIT;
 
+SET AUTOCOMMIT=0;
 INSERT INTO deaths (`user_id`, `room`, `killer`) VALUES
 (1, 'Big Battle', 'Sword Draugr'),
 (2, 'Hard Battle', 'Archer Draugr'),
@@ -64,6 +75,7 @@ INSERT INTO deaths (`user_id`, `room`, `killer`) VALUES
 (4, 'Hel Boss Room', 'Hel'),
 (5, 'Hel Boss Room', 'Hel'),
 (6, 'Hel Boss Room', 'Sword Draugr');
+COMMIT;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
