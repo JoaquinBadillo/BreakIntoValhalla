eccd084dce66ec2e17665f9eef1f2f276b502c22
Pboliopra <a01782428@tec.mx>
2023-05-26 22:12:41 -0600

    Renewed player scripts and behaviours with abstract class

---
+++ Game/Assets/Scripts/Player/Character.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour{
+    // Unity Objects
+    protected Animator animator; // Unity Object that allows script to change animation states
+    protected Rigidbody2D rigid2d; // Unity Object that makes it compatible with physics
+    // Physics
+    protected Vector2 movement;
+    // Attack Variables
+    public Transform meleeAttackPoint; // center of the gizmo
+    public float meleeRange; // gizmo radius
+    public LayerMask enemyLayers; // Layer that can be attacked by player
+    public LayerMask playerLayers;
+    protected bool immune = false;
+    // Stat variables
+    // HP
+    protected int maxHealth = 200;
+    protected int currentHealth;
+    // ATK
+    public int attack = 20; // PENDING protected variable
+    // ATKSPD
+    [SerializeField] protected float endLag; // Seconds before next attack can be executed
+    protected float timeUntilNextAttack;
+    // DEF
+    // SPD
+    [SerializeField] protected float speed = 2f;
+    
+
+    protected void Initialize(){
+        rigid2d = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        currentHealth = maxHealth;
+    }
+}
