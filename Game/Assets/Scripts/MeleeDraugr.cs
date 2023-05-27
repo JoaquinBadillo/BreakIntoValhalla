99bf71493aac9805b9177cdb04885ac863a7f541
Pboliopra <a01782428@tec.mx>
2023-05-26 22:15:05 -0600

    New Melee zombie behaviours

--- Game/Assets/Scripts/MeleeDraugr.cs
+++ Game/Assets/Scripts/MeleeDraugr.cs
@@ -3,98 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
-public class MeleeDraugr : MonoBehaviour{
-    // Unity Objects
-    private Animator animator;
-    private Rigidbody2D rigid2d;
+public class MeleeDraugr : Character{
+    // Astar Object
     public AIPath aiPath;
     // Attack Variables
-    // hitboxRange
-    private Transform meleeAttackPoint;
-    [SerializeField] float hitboxRange;
-    [SerializeField] LayerMask playerLayers;
     // reach
     [SerializeField] Transform player;
     [SerializeField] float range;
-    // Stat Variables
-    // HP
-    private int maxHealth = 150;
-    private int currentHealth;
-    // ATK
-    public int attack = 20;
-    // ATKSPD
-    public float attackRate = 2; // how many attacks per second
-    public float nextAttackTime; // 
-    // DEF
-    // SPD
+    // Slave script for animator events
+    private MeleeDraugrSpriter animatorSlave;
 
 
-    void Start(){
+    void Start() {
         rigid2d = GetComponent<Rigidbody2D>();
         animator = this.gameObject.transform.GetChild(0).GetComponent<Animator>();
+        animatorSlave = this.gameObject.transform.GetChild(0).GetComponent<MeleeDraugrSpriter>();
         currentHealth = maxHealth;
         animator.SetBool("isDead", false);
         GetComponent<AIPath>().enabled = false;
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    void Update(){
+    void Update() {
         animator.SetFloat("xSpeed", aiPath.desiredVelocity.x);
         animator.SetFloat("ySpeed", aiPath.desiredVelocity.y);
         Walk();
         Attack();
     }
 
-    void Walk(){
+    void Walk() {
         if (animator.GetFloat("xSpeed") > 0.01f){
             meleeAttackPoint = this.gameObject.transform.GetChild(2);
-            hitboxRange = 1f;
+            meleeRange = 1f;
         }
 
         else if (animator.GetFloat("xSpeed") < -0.01f){
             meleeAttackPoint = this.gameObject.transform.GetChild(1);
-            hitboxRange = 1f;
+            meleeRange = 1f;
         }
     }
 
-    void Attack(){
+    void Attack() {
         if (Vector3.Distance(transform.position, player.transform.position) < range){
             animator.SetTrigger("slash");
-            // Detecting attack range
-            Collider2D hitPlayer = Physics2D.OverlapCircle(meleeAttackPoint.position, hitboxRange, playerLayers);
-            // Deal damage
-            hitPlayer.GetComponent<PlayerMovement>().TakeDamage(attack);
+            timeUntilNextAttack = Time.time + endLag;
         }
     }
 
     void OnDrawGizmosSelected() {
         if (meleeAttackPoint == null) 
             return;
-        Gizmos.DrawWireSphere(meleeAttackPoint.position, hitboxRange);
+        Gizmos.DrawWireSphere(meleeAttackPoint.position, meleeRange);
     }
 
-    public void TakeDamage(int damage){
+    public void TakeDamage(int damage) {
         currentHealth -= damage;
         Debug.Log("Got hit, current health is now: " + currentHealth);
         if (currentHealth <= 0)
             Die();
     }
 
-    void Die(){
+    void Die() {
         animator.SetBool("isDead", true);
         Debug.Log("I died");
-        GetComponent<Collider2D>().enabled = false;
-        GetComponent<AIPath>().enabled = false;
-        this.enabled = false;
+        if (animatorSlave.endOfAnimation() == true)
+            this.gameObject.SetActive(false);
+        
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player"))
             GetComponent<AIPath>().enabled = true;
     }
+
     void OnTriggerExit2D(Collider2D other) {
         if (other.CompareTag("Player"))
             GetComponent<AIPath>().enabled = false;
     }
-    
 }
