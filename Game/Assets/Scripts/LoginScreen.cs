d5491615d5c6ed53e7d3303865842a9d0aa34590
Pboliopra <a01782428@tec.mx>
2023-05-30 01:49:55 -0600

    Login screen is 99% ready

---
+++ Game/Assets/Scripts/LoginScreen.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LoginScreen : MonoBehaviour {
+    EventSystem system;
+    public Selectable first;
+    public Button submit;
+    // Start is called before the first frame update
+    void Start() {
+        system = EventSystem.current;
+        first.Select();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+            if (next != null)
+                next.Select();
+            else
+                first.Select();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return)){
+            submit.onClick.Invoke();
+            Debug.Log("Gotta love the return key");
+        }
+    }
+}
