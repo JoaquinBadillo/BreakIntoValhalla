dca0bfa8dd69b04b3a215adaebb684c260da7460
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-05-30 20:08:10 -0600

    Update database schema

--- Database/valhalla-schema.sql
+++ Database/valhalla-schema.sql
@@ -1,5 +1,7 @@
 -- Valhalla Database Schema
--- Version 1.0
+-- Version 2.0
+-- Last edited May 30, 2023
+
 -- Developed by Einherjar / Joaquín Badillo, Pablo Bolio, Shaul Zayat
 
 
@@ -15,49 +17,42 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 -- MySQL will apply stricter validation rules and generate errors for operations that do not comply with these rules
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-
 DROP SCHEMA IF EXISTS valhalla;
 CREATE SCHEMA valhalla;
 USE valhalla;
-
---
--- Table structure for table `users`
---
-CREATE TABLE users (
-	user_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(30) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(32) NOT NULL,
-    PRIMARY KEY (user_id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
 
 --
 -- Table structure for table `metrics`
 --
 CREATE TABLE metrics (
 	metrics_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id SMALLINT UNSIGNED NOT NULL,
     kills SMALLINT UNSIGNED NOT NULL DEFAULT 0,
     wins SMALLINT UNSIGNED NOT NULL DEFAULT 0,
     last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (metrics_id),
-    UNIQUE INDEX user_id (user_id ASC),
-    CONSTRAINT fk_users_metrics FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
+    PRIMARY KEY (metrics_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 --
 -- Table structure for table `levels`
 --
 CREATE TABLE levels (
 	level_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    level_num TINYINT UNSIGNED NOT NULL,
-    -- In case the size of the number generated from the seed is large
-    seed MEDIUMINT UNSIGNED NOT NULL,
+    level_num TINYINT UNSIGNED NOT NULL DEFAULT 1,
+    seed MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
     PRIMARY KEY (level_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Table structure for table `characters`
+--
+CREATE TABLE characters (
+    character_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    class_id TINYINT UNSIGNED NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+    PRIMARY KEY (character_id),
+    INDEX class_id (class_id ASC),
+    CONSTRAINT fk_characters_classes FOREIGN KEY (class_id) REFERENCES classes (class_id) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Table structure for table `classes`
@@ -65,34 +60,18 @@ CREATE TABLE levels (
 CREATE TABLE classes (
 	class_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
-    PRIMARY KEY (class_id)
+    stats_id SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (class_id),
+    UNIQUE INDEX name (name ASC),
+    UNIQUE INDEX stats_id (stats_id ASC),
+    CONSTRAINT fk_classes_stats FOREIGN KEY (stats_id) REFERENCES stats (stats_id) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 --
--- Table structure for table `games`
---
-CREATE TABLE games (
-	game_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id SMALLINT UNSIGNED NOT NULL,
-    level_id TINYINT UNSIGNED NOT NULL,
-    class_id TINYINT UNSIGNED NOT NULL,
-    PRIMARY KEY (game_id),
-    UNIQUE INDEX user_id (user_id ASC),
-    UNIQUE INDEX level_id (level_id ASC),
-    INDEX class_id (class_id ASC),
-    CONSTRAINT fk_users_game FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT fk_levels_game FOREIGN KEY (level_id) REFERENCES levels (level_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT fk_classes_game FOREIGN KEY (class_id) REFERENCES classes (class_id) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
-
---
--- Table structure for table `sats`
+-- Table structure for table `stats`
 --
 CREATE TABLE stats (
 	stats_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    class_id TINYINT UNSIGNED NOT NULL,
     hp SMALLINT UNSIGNED NOT NULL,
     primary_attack SMALLINT UNSIGNED NOT NULL,
     secondary_attack SMALLINT NOT NULL,
@@ -100,9 +79,39 @@ CREATE TABLE stats (
     secondary_lag FLOAT NOT NULL,
     defense SMALLINT UNSIGNED NOT NULL,
     speed FLOAT NOT NULL,
-    PRIMARY KEY (stats_id),
-    UNIQUE INDEX class_id (class_id ASC),
-    CONSTRAINT fk_classes_stats FOREIGN KEY (class_id) REFERENCES classes (class_id) ON DELETE RESTRICT ON UPDATE CASCADE
+    PRIMARY KEY (stats_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `games`
+--
+CREATE TABLE games (
+	game_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    level_id TINYINT UNSIGNED NOT NULL,
+    character_id TINYINT UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (game_id),
+    UNIQUE INDEX level_id (level_id ASC),
+    INDEX character_id (character_id ASC),
+    CONSTRAINT fk_levels_game FOREIGN KEY (level_id) REFERENCES levels (level_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_character_id FOREIGN KEY (character_id) REFERENCES characters (character_id) ON DELETE RESTRICT
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `users`
+--
+CREATE TABLE users (
+	user_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(32) NOT NULL,
+    metrics_id SMALLINT UNSIGNED NOT NULL,
+    game_id SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (user_id),
+    UNIQUE INDEX metrics_id (metrics_id ASC),
+    UNIQUE INDEX game_id (game_id ASC),
+    UNIQUE INDEX username (username ASC),
+    CONSTRAINT fk_users_metrics FOREIGN KEY (metrics_id) REFERENCES metrics (metrics_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_users_game FOREIGN KEY (game_id) REFERENCES games (game_id) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
@@ -114,10 +123,9 @@ CREATE TABLE deaths(
     room VARCHAR(50) NOT NULL,
 	killer VARCHAR(50),
     PRIMARY KEY (deaths_id),
+    INDEX user_id (user_id ASC),
 	CONSTRAINT fk_deaths_users FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 SHOW TABLES;
 
