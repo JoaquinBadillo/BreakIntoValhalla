8876758ec39a8194e795f4bd87b6a1d489b76651
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-06-15 00:24:10 -0600

    We fetching

--- Web/public/assets/js/stats.js
+++ Web/public/assets/js/stats.js
@@ -1,13 +1,18 @@
 const URI = "https://valhallaapi-production.up.railway.app";
 
+isFloat = (n) => {
+    return Number(n) === n && n % 1 !== 0;
+}
+
 fetch(`${URI}/api/characters/stats`).then(res => res.json())
 .then(res => {
     res.map((character, index) => {
         for (let attribute in character){
             let selection = document.querySelectorAll(`#${attribute}`);
-
-            if (selection.length !== 0)
-                selection[index] = character[attribute];
+            
+            let value = character[attribute];
+            if (selection.length !== 0)           
+                selection[index].innerHTML = isFloat(value) ? value.toFixed(2) : value;
         }
     })
 }).catch(err => {
